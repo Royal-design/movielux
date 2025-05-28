96cc39298733145817b183b72b23ac91363c1d31
Royal-design <royaldesign1999@gmail.com>
2025-05-28 16:32:58 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl">hello</p>
+      <Button className="bg-green-200 hover:bg-green-600/15 transition-colors">
+        Test
+      </Button>
+    </>
+  );
+}
+
+export default App;
